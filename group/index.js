355da87dce0b2a2e469f3fd93115c2aef0a3223f
Mahajaya <mahajaya@Mahajayas-Air.home>
2022-06-12 11:22:08 -0400

    Soma-Hello-World

--- group/index.js
+++ group/index.js
@@ -3,6 +3,8 @@ function fn(...c) {
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a + b);
   }
+
+  Hello World!
   
   fn(1, 2, 3); //> 6
   fn(10, "B", 20); //> error All arguments must be numbers.
